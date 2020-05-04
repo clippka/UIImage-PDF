e10849d432908d7ddaccd83c6d8b9113eb8b3e9c
Christian Lippka <christian.lippka@numberfour.eu>
2020-05-04 18:18:38 +0200

    renamed PDFView to NBPDFView due to name clash

--- UIImage+PDF/NBPDFView.m
+++ UIImage+PDF/NBPDFView.m
@@ -1,14 +1,14 @@
 //
-//  PDFView.m
+//  NBPDFView.m
 //
 //  Created by Nigel Barber on 15/10/2011.
 //  Copyright 2011 Mindbrix Limited. All rights reserved.
 //
 
-#import "PDFView.h"
+#import "NBPDFView.h"
 
 
-@implementation PDFView
+@implementation NBPDFView
 
 @synthesize page = m_page;
 @synthesize resourceName = m_resourceName;
@@ -33,7 +33,7 @@
 {
 	m_resourceName = resourceName;
 	
-    self.resourceURL = [ PDFView resourceURLForName: self.resourceName ];
+    self.resourceURL = [ NBPDFView resourceURLForName: self.resourceName ];
 }
 
 
@@ -54,7 +54,7 @@
 
 +(CGRect) mediaRect:(NSString *)resourceName
 {
-    return [ PDFView mediaRectForURL:[ PDFView resourceURLForName: resourceName ]];
+    return [ NBPDFView mediaRectForURL:[ NBPDFView resourceURLForName: resourceName ]];
 }
 
 
